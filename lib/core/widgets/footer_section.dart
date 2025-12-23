0cb7d0f2b5c2700978cf143e32d171f03db0597e
Mohit Rajpurohit <mrmohitrajpurohit@gmail.com>
2025-12-23 22:01:28 +0530

    Make footer responsive for mobile and desktop
    Refactored FooterSection to provide separate layouts for mobile and desktop, adjusting padding, height, and arrangement of elements based on screen width. Also reduced horizontal margin in ContactInfo for mobile devices to improve layout on smaller screens.

--- lib/core/widgets/footer_section.dart
+++ lib/core/widgets/footer_section.dart
@@ -12,9 +12,15 @@ class FooterSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final isMobile = width < 700;
+
     return Container(
-      height: isSticky ? 80 : null,
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
+      height: isSticky ? (isMobile ? 60 : 72) : null,
+      padding: EdgeInsets.symmetric(
+        horizontal: isMobile ? 16 : 24,
+        vertical: isMobile ? 10 : 14,
+      ),
       decoration: BoxDecoration(
         color: Colors.white,
         border: Border(
@@ -33,60 +39,8 @@ class FooterSection extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          // ================= MAIN ROW =================
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // LEFT: NAME
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "Mohit",
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  Text(
-                    "Software Developer",
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.black54,
-                    ),
-                  ),
-                ],
-              ),
-
-              // CENTER: SOCIAL ICONS
-              Row(
-                children: [
-                  _socialIcon(
-                    iconPath: 'assets/icons/github_icon.png',
-                    onTap: () => _open('https://github.com/mohit-01-code'),
-                  ),
-                  _socialIcon(
-                    iconPath: 'assets/icons/linkdin_icon.png',
-                    onTap: () =>
-                        _open('https://www.linkedin.com/in/mohit-rajpurohit-8b9ba41a3/'),
-                  ),
-                  _socialIcon(
-                    iconPath: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',
-                    onTap: () =>
-                        _open('https://www.instagram.com/_mohit_rajguru/'),
-                  ),
-                ],
-              ),
-
-              // RIGHT: RESUME
-              TextButton.icon(
-                onPressed: () =>
-                    _open('assets/files/Mohit_Resume.pdf'),
-                icon: const Icon(Icons.download, size: 16),
-                label: const Text("Resume"),
-              ),
-            ],
-          ),
+          // ================= MAIN CONTENT =================
+          isMobile ? _mobileLayout() : _desktopLayout(),
 
           // ================= COPYRIGHT (ONLY SCROLL MODE) =================
           if (!isSticky) ...[
@@ -104,13 +58,122 @@ class FooterSection extends StatelessWidget {
     );
   }
 
+  // ================= DESKTOP LAYOUT =================
+  Widget _desktopLayout() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        // LEFT: NAME
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              "Mohit",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              "Software Developer",
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.black54,
+              ),
+            ),
+          ],
+        ),
+
+        // CENTER: SOCIAL ICONS
+        Row(
+          children: [
+            _socialIcon(
+              iconPath: 'assets/icons/github_icon.png',
+              onTap: () => _open('https://github.com/mohit-01-code'),
+            ),
+            _socialIcon(
+              iconPath: 'assets/icons/linkdin_icon.png',
+              onTap: () => _open(
+                'https://www.linkedin.com/in/mohit-rajpurohit-8b9ba41a3/',
+              ),
+            ),
+            _socialIcon(
+              iconPath:
+              'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',
+              onTap: () =>
+                  _open('https://www.instagram.com/_mohit_rajguru/'),
+            ),
+          ],
+        ),
+
+        // RIGHT: RESUME
+        TextButton.icon(
+          onPressed: () => _open('assets/files/Mohit_Resume.pdf'),
+          icon: const Icon(Icons.download, size: 16),
+          label: const Text("Resume"),
+        ),
+      ],
+    );
+  }
+
+  // ================= MOBILE LAYOUT =================
+  Widget _mobileLayout() {
+    return Column(
+      children: [
+        // NAME + RESUME
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Mohit",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            IconButton(
+              tooltip: "Download Resume",
+              icon: const Icon(Icons.download),
+              onPressed: () => _open('assets/files/Mohit_Resume.pdf'),
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 6),
+
+        // SOCIAL ICONS
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _socialIcon(
+              iconPath: 'assets/icons/github_icon.png',
+              onTap: () => _open('https://github.com/mohit-01-code'),
+            ),
+            _socialIcon(
+              iconPath: 'assets/icons/linkdin_icon.png',
+              onTap: () => _open(
+                'https://www.linkedin.com/in/mohit-rajpurohit-8b9ba41a3/',
+              ),
+            ),
+            _socialIcon(
+              iconPath:
+              'https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png',
+              onTap: () =>
+                  _open('https://www.instagram.com/_mohit_rajguru/'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
   // ================= SOCIAL ICON =================
   Widget _socialIcon({
     required String iconPath,
     required VoidCallback onTap,
   }) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 6),
       child: InkWell(
         onTap: onTap,
         borderRadius: BorderRadius.circular(50),
@@ -120,11 +183,15 @@ class FooterSection extends StatelessWidget {
             shape: BoxShape.circle,
             border: Border.all(color: Colors.grey.shade300),
           ),
-          child: iconPath.contains('http')? Image.network(iconPath,
+          child: iconPath.contains('http')
+              ? Image.network(
+            iconPath,
             height: 18,
             width: 18,
             errorBuilder: (_, __, ___) =>
-            const Icon(Icons.link, size: 18),) :Image.asset(
+            const Icon(Icons.link, size: 18),
+          )
+              : Image.asset(
             iconPath,
             height: 18,
             width: 18,
